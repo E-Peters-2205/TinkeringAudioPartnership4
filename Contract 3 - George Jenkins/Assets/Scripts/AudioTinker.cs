5dfe3730d2f39676fec129c72f34aa1d49a4297b
gj241211 <gj241211@falmouth.ac.uk>
2019-11-27 18:12:36 +0000

    Merged UI Unity Project with Tinkering Audio contract 3 to implement a way of playing different melodies. Edited Audio Tinker script, setting up variable to tinker with. Added many comments.

--- Contract 3 - George Jenkins/Assets/Scripts/AudioTinker.cs
+++ Contract 3 - George Jenkins/Assets/Scripts/AudioTinker.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 //using NaughtyAttributes;
 using UnityEngine;
+using System;
 
 #if UNITY_EDITOR
 using UnityEditor;
@@ -11,12 +12,21 @@ using UnityEditor;
 public class AudioTinker : MonoBehaviour {
     private AudioSource audioSource;
     private AudioClip outAudioClip;
+    // Variables to change notes ect
+    int Frequancy = 1500;
+    int Duration = 1;
+
+
+    // sets notes
+
+    int Note_C = (1047);
+    int Note_D = (1175);
     
     
     // Start is called before the first frame update
     void Start() {
         audioSource = GetComponent<AudioSource>();
-        outAudioClip = CreateToneAudioClip(1500);
+        outAudioClip = CreateToneAudioClip(Frequancy,Duration);
     }
 
     void Update()
@@ -36,32 +46,29 @@ public class AudioTinker : MonoBehaviour {
     
     
     // Private 
-    private AudioClip CreateToneAudioClip(int frequency) {
-        int sampleDurationSecs = 5;
+    private AudioClip CreateToneAudioClip(Frequency, Duration) {
+        int sampleDurationSecs = Duration;
         int sampleRate = 44100;
-        int sampleLength = sampleRate * sampleDurationSecs;
-        float maxValue = 1f / 4f;
+        int sampleLength = sampleRate * sampleDurationSecs;              //Above sets variables that will be used to create the tone.
+        float maxValue = 1f / 4f;                                        // max value is a multiplyer to evenly change every audio sample. 
         
         var audioClip = AudioClip.Create("tone", sampleLength, 1, sampleRate, false);
         
         float[] samples = new float[sampleLength];
-        for (var i = 0; i < sampleLength; i++) {
-            float s = Mathf.Sin(2.0f * Mathf.PI * frequency * ((float) i / (float) sampleRate));
-            float v = s * maxValue;
-            samples[i] = v;
+        for (var i = 0; i < sampleLength; i++) {                                                     //for loop to go through all samples in the tone and create their audio number equivilent.
+            float s = Mathf.Sin(2.0f * Mathf.PI * Frequency * ((float) i / (float) sampleRate));     // takes all the input data and creates the sound number for that sample
+            float v = s * maxValue;                                                                  // applys the multiplyer to the sample
+            samples[i] = v;                                                                          // an array of all the audio number in the order they are played.
         }
 
-        audioClip.SetData(samples, 0);
-        return audioClip;
+        audioClip.SetData(samples, 0);  //telling the samples that they represent adio numbers.
+        return audioClip; 
     }
 
-    
-#if UNITY_EDITOR
-    //[Button("Save Wav file")]
-    private void SaveWavFile() {
-        string path = EditorUtility.SaveFilePanel("Where do you want the wav file to go?", "", "", "wav");
-        var audioClip = CreateToneAudioClip(1500);
-        SaveWavUtil.Save(path, audioClip);
+    // Below sets simple melody.
+
+    private void CreateMelody() {
+
     }
-#endif
+
 }
