059ea528a8675d3768606e2565eddcde821db29a
gj241211 <gj241211@falmouth.ac.uk>
2019-11-27 11:29:58 +0000

    separated contract 4 to prepare adding contract 3

---
+++ Contract 4 Edward Peters/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+
+    public bool someBool;
+    public bool someOtherBool;
+    public int counter = 0;
+
+    public void DoSomething()
+    {
+        Debug.Log("Doing something!");
+        someBool = !someBool;
+    }
+
+}
