21b5d0a0281418b9c5bfe9f7d7058732e0ee248b
gj241211 <gj241211@falmouth.ac.uk>
2019-11-27 21:52:19 +0000

    Edited Contract 3 script to set up basic melody generation with loops. (loop constructs only melody will not play).

--- Contract 3 - George Jenkins/Assets/Scripts/AudioTinker.cs
+++ Contract 3 - George Jenkins/Assets/Scripts/AudioTinker.cs
@@ -13,20 +13,21 @@ public class AudioTinker : MonoBehaviour {
     private AudioSource audioSource;
     private AudioClip outAudioClip;
     // Variables to change notes ect
-    int Frequancy = 1500;
+    
     int Duration = 1;
 
 
     // sets notes
+    int Frequancy = 1500;
 
     int Note_C = (1047);
     int Note_D = (1175);
-    
-    
+
+
     // Start is called before the first frame update
     void Start() {
         audioSource = GetComponent<AudioSource>();
-        outAudioClip = CreateToneAudioClip(Frequancy,Duration);
+        outAudioClip = CreateToneAudioClip(Frequancy, Duration);
     }
 
     void Update()
@@ -46,13 +47,13 @@ public class AudioTinker : MonoBehaviour {
     
     
     // Private 
-    private AudioClip CreateToneAudioClip(Frequency, Duration) {
+    private AudioClip CreateToneAudioClip(int Frequency, int Duration) {
         int sampleDurationSecs = Duration;
         int sampleRate = 44100;
         int sampleLength = sampleRate * sampleDurationSecs;              //Above sets variables that will be used to create the tone.
         float maxValue = 1f / 4f;                                        // max value is a multiplyer to evenly change every audio sample. 
         
-        var audioClip = AudioClip.Create("tone", sampleLength, 1, sampleRate, false);
+        var audioToneClip = AudioClip.Create("tone", sampleLength, 1, sampleRate, false);
         
         float[] samples = new float[sampleLength];
         for (var i = 0; i < sampleLength; i++) {                                                     //for loop to go through all samples in the tone and create their audio number equivilent.
@@ -61,14 +62,39 @@ public class AudioTinker : MonoBehaviour {
             samples[i] = v;                                                                          // an array of all the audio number in the order they are played.
         }
 
-        audioClip.SetData(samples, 0);  //telling the samples that they represent adio numbers.
-        return audioClip; 
+        audioToneClip.SetData(samples, 0);  //telling the samples that they represent audio numbers.
+        return audioToneClip; 
     }
 
     // Below sets simple melody.
 
     private void CreateMelody() {
 
+        int count = 0;
+        float[] Melody_Chunks = new float[Length]; // what will be the length of this? how do i define it before all have been added without "hard coding"?
+
+
+        while (count < 5)
+        {
+
+            CreateToneAudioClip(Note_C,Duration);                //gets the audio numbers for first note in this case C and stores it in an array 
+
+
+            CreateToneAudioClip(Note_D,Duration);                //gets audio numbers for second Note 
+
+
+
+            count++;
+        }
+
+
+        var audioClip = AudioClip.Create("tone", sampleLength, 1, sampleRate, false);
+
+        for (var c = 0; c < Melody_Chunks.Length; c++) {       // seting up a loop to add all the tones together ino a single audio clip 
+            
+            audioClip.SetData(Melody_Chunks[c], c);              
+
+        }
     }
 
 }
