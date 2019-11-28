df9f2bf8768f6fac3bab0607baa4580022224c15
gj241211 <gj241211@falmouth.ac.uk>
2019-11-28 15:57:49 +0000

    Contract 3
    Edited unity UI to add a stop button.
    Edited the AudioTinker script to allow a simple melody to be played
    Also Added random assignment to frequency's

--- Contract 3 - George Jenkins/Assets/Scripts/AudioTinker.cs
+++ Contract 3 - George Jenkins/Assets/Scripts/AudioTinker.cs
@@ -17,7 +17,7 @@ public class AudioTinker : MonoBehaviour {
     int Duration = 1;
 
 
-    // sets notes
+    // the frquancy of notes
     int Frequancy = 1500;
 
     int Note_C = (1047);
@@ -27,12 +27,15 @@ public class AudioTinker : MonoBehaviour {
     // Start is called before the first frame update
     void Start() {
         audioSource = GetComponent<AudioSource>();
-        outAudioClip = CreateToneAudioClip(Frequancy, Duration);
+        //outAudioClip = CreateToneAudioClip(Frequancy, Duration);
+
+        CreateMelody();
+
     }
 
     void Update()
     {
-        PlayOutAudio();
+        //PlayOutAudio();
     }    
 
     // Public APIs
@@ -62,32 +65,71 @@ public class AudioTinker : MonoBehaviour {
             samples[i] = v;                                                                          // an array of all the audio number in the order they are played.
         }
 
-        audioToneClip.SetData(samples, 0);  //telling the samples that they represent audio numbers.
-        return audioToneClip; 
+        audioToneClip.SetData(samples, 0);         //telling the samples that they represent audio numbers.
+        return (audioToneClip); 
     }
 
     // Below sets simple melody.
 
     private void CreateMelody() {
 
-        int count = 0;
-        float[] Melody_Chunks = new float[Length]; // what will be the length of this? how do i define it before all have been added without "hard coding"?
 
+        List<AudioClip> Audio_Clips = new List<AudioClip>();    //makes an empty list to add future clips too
+        
+        int count = 0;                                          // a counter for the loop
 
-        while (count < 5)
+        System.Random r = new System.Random(); 
+
+        while (count < 10)                                      // a loop to create different notes
         {
+            int freq = 440;                                     // creates a variable to store temopary freqancy
 
-            CreateToneAudioClip(Note_C,Duration);                //gets the audio numbers for first note in this case C and stores it in an array 
+            if(r.Next(10) % 2 == 0)                             // number checker to see if odd or even, this changes the note that is played  
+            {
+                freq = Note_C;                           
+            }
+            else
+            {
+                freq = Note_D;
+            }
 
-
-            CreateToneAudioClip(Note_D,Duration);                //gets audio numbers for second Note 
-
-
-
+            Audio_Clips.Add(CreateToneAudioClip(freq, Duration));                //gets the audio numbers for first note in this case C and stores it in an array 
+            //Audio_Clips.Add(CreateToneAudioClip(Note_D,Duration));                //gets audio numbers for second Note 
+                                                                                  //Audio_Clips.Add(CreateToneAudioClip(220*count, Duration));  
             count++;
         }
 
+        int targetSamples = 0;                   // sets a variable to 0 to count the samples
 
+        foreach (AudioClip note in Audio_Clips)  // this loop counts the samples 
+        {
+            targetSamples += note.samples;           
+        }
+
+        
+
+        AudioClip melodyClip = AudioClip.Create("melody", targetSamples, 1, 44100, false);  // creates a melody from all the samples
+
+        int offsetPosition = 0;                                     //this variable is used to mark the start of an audio clip 
+
+        foreach (AudioClip note in Audio_Clips)                     // a loop to add the audio clip to melody clip in order
+        {
+            
+
+            float[] samples = new float[note.samples];
+            note.GetData(samples, 0);
+
+            melodyClip.SetData(samples, offsetPosition);
+
+            offsetPosition += note.samples;
+        }
+
+        audioSource.PlayOneShot(melodyClip);
+
+
+        // need to save the audio file 
+
+        /*
         var audioClip = AudioClip.Create("tone", sampleLength, 1, sampleRate, false);
 
         for (var c = 0; c < Melody_Chunks.Length; c++) {       // seting up a loop to add all the tones together ino a single audio clip 
@@ -95,6 +137,7 @@ public class AudioTinker : MonoBehaviour {
             audioClip.SetData(Melody_Chunks[c], c);              
 
         }
+        */
     }
 
 }
